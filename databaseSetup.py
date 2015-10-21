35d7b29580a8c92b1f9a5627e6011779a24ce4bf
stavskal <stavrianos_@hotmail.com>
2015-10-21 14:16:47 +0200

    Database creation script updated for call_logs,sms,app_usage,calendar

--- databaseSetup.py
+++ databaseSetup.py
@@ -2,32 +2,29 @@ import csv,codecs,os
 from pymongo import MongoClient
 
 
-global_mongo = None
-global_db = None
-global_coll = None
-
 client = MongoClient()
-db = client.AppUsage
+db = client.dataset
 
 
-def dbSetup(csvfile):	
-	i=0
+def dbInsertData(csvfile,c):	
 	with open(csvfile,'rb') as inCsv:
 		parsed = csv.DictReader(inCsv , delimiter = ',' , quotechar='"')
 		for record in parsed:
-			#string_record=dict([(str(k), v) for k, v in record.items()])
-			add_record_to_mongo(record,i)
-			i= i+1
+
+			#inserting data in to proper collection inside DB
+			if c == 'app_usage':
+				db.app_usage.insert_one(record)
+
+			elif c == 'sms':
+				db.sms.insert_one(record)
+
+			elif c == 'call_log':
+				db.call_log.insert_one(record)
+
+			elif c == 'calendar':
+				db.calendar.insert_one(record)
+
 			
-
-
-
-#code from github/manchicken
-def add_record_to_mongo(record,i):
-	
-	print i
-	# Now let's insert
-	db.logs.insert_one(record)
 
 
 
@@ -35,13 +32,51 @@ def add_record_to_mongo(record,i):
 
 def main():
 
-	#current directory script being run
+	#Below four folders containing app_usage,sms,call_logs,calendar are being
+	#loaded and inserted RAW in the database for further processing
+
+
+	#setting directory to load app_usage information
 	directory = os.path.dirname(os.path.abspath(__file__)) + '/dataset/app_usage'
-	
+
+	#inserting all files to database
 	for filename in os.listdir(directory):
 		filename= directory +'/'+ filename
+		dbInsertData(filename,'app_usage')
 
-		dbSetup(filename)
+	print('Done with app_usage')
+	#setting directory to load sms information
+	directory = os.path.dirname(os.path.abspath(__file__)) + '/dataset/sms'
+
+	#inserting all files to database
+	for filename in os.listdir(directory):
+		filename= directory +'/'+ filename
+		dbInsertData(filename,'sms')
+
+
+	print('Done with sms')
+
+	#setting directory to load sms information
+	directory = os.path.dirname(os.path.abspath(__file__)) + '/dataset/call_log'
+
+	#inserting all files to database
+	for filename in os.listdir(directory):
+		filename= directory +'/'+ filename
+		dbInsertData(filename,'call_log')
+
+	print('Done with call_logs')
+	#setting directory to load sms information
+	directory = os.path.dirname(os.path.abspath(__file__)) + '/dataset/calendar'
+
+	#inserting all files to database
+	for filename in os.listdir(directory):
+		filename= directory +'/'+ filename
+		dbInsertData(filename,'calendar')
+
+	print('Done with calendar')
+
+
+
 
 
 if __name__ == '__main__':
