2e7f4b2b407d59208d4410aa533166b60d8260d2
stavskal <stavrianos_@hotmail.com>
2015-10-22 13:19:55 +0200

    Added function countAppOccur that counts how many times each application is used by given user

--- processingFunctions.py
+++ processingFunctions.py
@@ -1,12 +1,17 @@
-import datetime
-
+import datetime,psycopg2
+from collections import Counter
 #converts unix timestamp to human readable date (e.g '1234567890' -> '2009-02-14  00:31:30')
-def UnixTimeConv(timestamp):
+def unixTimeConv(timestamp):
 	newTime=str(datetime.datetime.fromtimestamp(int(timestamp)))
 	year,time=newTime.split(' ')
 	#z=x[2].split(':')
 	return (year,time)
 
+#counts occurence of each app for given user 'uid'
+def countAppOccur(cur,uid):
+	cur.execute("""SELECT running_task_id  FROM appusage WHERE uid = %s ; """, [uid] )
+	records = cur.fetchall()
+	return Counter(records)
 
-a=UnixTimeConv(1234567890)
-print(a)
+
+
