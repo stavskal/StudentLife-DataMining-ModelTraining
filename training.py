cd6e3cddf2bcb9ba3e5fede79298a18fb02b0730
stavskal <stavrianos_@hotmail.com>
2015-10-29 16:01:09 +0100

    appStatsL now accurately computes feature vector over day period, with fixed length for each user

--- training.py
+++ training.py
@@ -10,20 +10,39 @@ uids = ['u00','u01','u02','u03','u04','u05','u07','u08','u09','u10','u12','u13',
 'u25','u27','u30','u31','u32','u33','u34','u35','u36','u39','u41','u42','u43','u44','u45','u46','u47','u49','u50','u51','u52','u53','u54',
 'u56','u57','u58','u59']
 
+
+
+
+
+
 # returns feature vector corresponing to 'timestamp' stress report (label)
+# This feature vector is of size len(uniqueApps), total number of different apps for user
+# each cell corresponds to the % of time for one app. Apps that were not used during 
+# previous day simply have zero in feature vector cell
 def appStatsL(cur,uid,timestamp):
 	appOccurTotal = countAppOccur(cur,uid)
-	print(len(appOccurTotal))
+	keys = np.fromiter(iter(appOccurTotal.keys()), dtype=int)
+	appStats1 = np.zeros(len(keys))
 	appStats=[]
 	
 	tStart = timestamp - 2*day
 
 	cur.execute("""SELECT running_task_id  FROM appusage WHERE uid = %s AND time_stamp > %s AND time_stamp < %s ; """, [uid,tStart,timestamp] )
 	records= Counter( cur.fetchall() )
-
-	#transforming keys to be in readable format
 	for k in records.keys():
-		records[k[0]] = records.pop(k)	
+		records[k[0]] = records.pop(k)
+
+
+	for i in keys:
+		if i in records.keys():
+			print(records[i])
+			print(appOccurTotal[i])
+			appStats1[i] = float(records[i])*100 / float(appOccurTotal[i])
+	print(appStats1)
+
+	
+
+
 
 	# number of unique applications 
 	uniqueApps = len(records.keys())
@@ -32,6 +51,9 @@ def appStatsL(cur,uid,timestamp):
 	appStats.append(usageFrequency)
 
 	return uniqueApps
+
+
+
 
 #---------------------------------------------------------------------
 # computes the total time (sec) that screen was on during the past day
@@ -59,9 +81,9 @@ def timeScreenOn(cur,uid,timestamp):
 
 con = psycopg2.connect(database='dataset', user='tabrianos')
 cur = con.cursor()
-timeScreenOn(cur,'u12',1365809051)
+#timeScreenOn(cur,'u12',1365809051)
 #for i in uids:
-#	print(appStatsL(cur,i,1366248106))	
+appStatsL(cur,'u00',1366388106)
 
 
 
