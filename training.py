abe6d4e59d10688bd1920d6574e1a19f8bf14b64
stavskal <stavrianos_@hotmail.com>
2015-10-29 14:54:05 +0100

    Modified appStatsL to return most freq apps(not 100% working) and added function that computes total time screen was on during the past day

--- training.py
+++ training.py
@@ -1,56 +1,71 @@
 import json,csv,sys,os,psycopg2
+import numpy as np
+from collections import Counter 
 from processingFunctions import  computeAppStats, countAppOccur, appTimeIntervals
 
 
 
 day = 86400
+uids = ['u00','u01','u02','u03','u04','u05','u07','u08','u09','u10','u12','u13','u14','u15','u16','u17','u18','u19','u20','u22','u23','u24',
+'u25','u27','u30','u31','u32','u33','u34','u35','u36','u39','u41','u42','u43','u44','u45','u46','u47','u49','u50','u51','u52','u53','u54',
+'u56','u57','u58','u59']
 
-
-
-def computeAppStats(cur,uid,timestamp):
+# returns feature vector corresponing to 'timestamp' stress report (label)
+def appStatsL(cur,uid,timestamp):
 	appOccurTotal = countAppOccur(cur,uid)
+	print(len(appOccurTotal))
 	appStats=[]
-
-	i=0
 	
-	while(tStart+timeWin<tEnd):
-		tAfter = tStart + timeWin
-		cur.execute("""SELECT running_task_id  FROM appusage WHERE uid = %s AND time_stamp > %s AND time_stamp < %s ; """, [uid,tStart,tAfter] )
-		records= Counter( cur.fetchall() )
+	tStart = timestamp - 2*day
 
-		#transforming keys to be in readable format
-		for k in records.keys():
-			records[k[0]] = records.pop(k)
+	cur.execute("""SELECT running_task_id  FROM appusage WHERE uid = %s AND time_stamp > %s AND time_stamp < %s ; """, [uid,tStart,timestamp] )
+	records= Counter( cur.fetchall() )
 
-		# number of unique applications 
-		uniqueApps = len(records.keys())
-		
-		# usageFrequency:  number of times in timeWin / total times
-		usageFrequency= {k: float(records[k])*100/float(appOccur[k]) for k in appOccur.viewkeys() & records.viewkeys() }
-		
+	#transforming keys to be in readable format
+	for k in records.keys():
+		records[k[0]] = records.pop(k)	
 
-		appStats.append(usageFrequency)
-		
-		tStart = tStart + timeWin
-		tAfter = tAfter + timeWin
+	# number of unique applications 
+	uniqueApps = len(records.keys())
+	# usageFrequency:  number of times in timeWin / total times
+	usageFrequency= {k: float(records[k])*100/float(appOccurTotal[k]) for k in appOccurTotal.viewkeys() & records.viewkeys() }
+	appStats.append(usageFrequency)
 
-		i=i+1
+	return uniqueApps
 
-	return appStats
+#---------------------------------------------------------------------
+# computes the total time (sec) that screen was on during the past day
+def timeScreenOn(cur,uid,timestamp):
+	uid = uid +'dark'
+	#tStart is exactly 24h before given timestamp
+	tStart = timestamp - day
 
+	#fetching all records that fall within this time period
+	cur.execute('SELECT timeStart,timeStop FROM {0} WHERE timeStart >= {1} AND timeStop <= {2}'.format(uid,tStart,timestamp))
+	records = cur.fetchall()
 
+	totalTime =0
+	for k in records:
+		totalTime += k[1]-k[0]
 
-
-
-
-
-
-
-
-
+	return totalTime
 
 
 
+
+
+
+con = psycopg2.connect(database='dataset', user='tabrianos')
+cur = con.cursor()
+timeScreenOn(cur,'u12',1365809051)
+#for i in uids:
+#	print(appStatsL(cur,i,1366248106))	
+
+
+
+
+
+"""
 def main(argv):
 
 	#connecting to database
@@ -102,3 +117,7 @@ def main(argv):
 
 if __name__ == '__main__':
 	main()
+
+
+	"""
+
