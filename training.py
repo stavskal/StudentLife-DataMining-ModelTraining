18e285b1f456857cb680f11abd90fcf477c0009d
stavskal <stavrianos_@hotmail.com>
2015-10-30 17:31:16 +0100

    Cross-validation and documentation added. Also average statistics over users

--- training.py
+++ training.py
@@ -14,7 +14,7 @@ uids = ['u00','u01','u02','u03','u04','u05','u07','u08','u09','u10','u12','u13',
 'u25','u27','u30','u31','u32','u33','u34','u35','u36','u39','u41','u42','u43','u44','u45','u46','u47','u49','u50','u51','u52','u53','u54',
 'u56','u57','u58','u59']
 
-uids1=['u10','u16','u19','u32','u33','u43','u44','u49','u57','u59']
+uids1=['u10','u16','u19','u32','u33','u43','u44','u36','u57','u59']
 
 
 
@@ -85,6 +85,8 @@ def timeScreenOn(cur,uid,timestamp):
 con = psycopg2.connect(database='dataset', user='tabrianos')
 cur = con.cursor()
 acc=0
+totalP=0
+totalR=0
 
 
 # ------------TEST CASE-----------------------------
@@ -148,16 +150,24 @@ for testUser in uids1:
 
 	output = forest.predict(Xtest)
 	
-	metricP = precision_score(Ytest,output, average='weighted')
-	metricR = recall_score(Ytest,output, average='weighted')
+	metricP = precision_score(Ytest,output, average='binary')
+	metricR = recall_score(Ytest,output, average='binary')
 
 	tempAcc = forest.score(Xtest,Ytest)
+
+	totalP += metricP
+	totalR +=metricR
 	acc += tempAcc
 	print('P,R: {0} , {1} '.format(metricP,metricR))
 	print('Fscore: {0}'.format ( 2*metricP*metricR/(metricR+metricP)))
 	print('Accuracy: {0} %'.format(tempAcc*100))
 
 print('Average accuracy: {0} %'.format(float(acc)*100/len(uids1)))
+print('Average precision: {0} %'.format(float(totalP)*100/len(uids1)))
+print('Average recall: {0} %'.format(float(totalR)*100/len(uids1)))
+
+
+
 """
 
 
