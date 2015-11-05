3b752c1d123dd80b5c105ac41bc89c0797019e56
stavskal <stavrianos_@hotmail.com>
2015-11-05 11:53:41 +0100

    Min/average/max accuracy for user-specific window with NMCA E[35,120]

--- dataVis.py
+++ dataVis.py
@@ -7,8 +7,8 @@ from processingFunctions import *
 
 sns.set_style('darkgrid')
 sns.set(color_codes=True)
-heat = np.array([[62.06,63.34, 60.70,60.8 ,54],[60.44,61.14, 55.38, 63.99, 60.05],[67.1,60.6, 60.54,57.52,58.14]])
-#print(heat)
+#heat = np.array([[62.06,63.34, 60.70,60.8 ,54],[60.44,61.14, 55.38, 63.99, 60.05],[67.1,60.6, 60.54,57.52,58.14]])
+#print(heat.shape)
 
 """
 ax1 = sns.heatmap(heat, vmin=50, vmax=65)
@@ -25,16 +25,29 @@ sb = ax1.get_figure()
 sb.savefig('heatmap2.png')
 print('done baby')
 
-"""
-
 pyp.figure()
-y1=[62.98, 58.8, 61.25, 60.07, 60.09, 61.11 ,55.2]
+y1=np.array([]   [62.98, 58.8, 61.25, 60.07, 60.09, 61.11 ,55.2])
 x1 = [0,1,2,3,4,5,6]
 xtic = ['100','70','60','50','40','30','20']
 pyp.xticks(x1,xtic)
 pyp.title('Accuracy performance with User-Specific Time Window')
 pyp.xlabel('No. of Most Common Apps kept')
 pyp.ylabel('Averaged accuracy over Users (%)')
-ax = sns.tsplot(data=y1)
+
+ax = sns.tsplot(data=y1, estimator=np.median)
 fig = ax.get_figure()
-fig.savefig('meanStressWin.png')
+fig.savefig('meanStressWinTRY.png')
+
+
+"""
+#               max             		average            			min
+y = np.array([[70.3,74,69.2,69.2,84.6],[61.4,52.6,54.6,50.1,58.2],[50,31.8,41.66,50,25]])
+x=[0,1,2,3,4]
+xtic=['120','100','70','50','35']
+pyp.xticks(x, xtic)
+pyp.title('Accuracy performance with User-Specific Time Window')
+pyp.xlabel('No. of Most Common Apps kept')
+pyp.ylabel('Accuracy over Users (%)')
+ax = sns.tsplot(data=y)
+fig = ax.get_figure()
+fig.savefig('coolplot.png')
