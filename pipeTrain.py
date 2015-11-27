8bca6cb8186d9412ea4a1095d739bc3feb1cc9e5
stavskal <stavrianos_@hotmail.com>
2015-11-27 15:22:31 +0100

    Moved Custom Ensemble Classifier from training.py (not working yet)

--- pipeTrain.py
+++ pipeTrain.py
@@ -2,7 +2,7 @@ import json,csv,sys,os,psycopg2,random
 import numpy as np
 from collections import Counter 
 from processingFunctions import *
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.metrics import precision_score, recall_score, confusion_matrix
 from sklearn.cross_validation import cross_val_predict, StratifiedKFold, KFold,cross_val_score, LeaveOneLabelOut
 from sklearn.pipeline import Pipeline
@@ -17,6 +17,8 @@ def tolAcc(y,pred):
 	"""Returns accuracy as defined by the Tolerance Method
 	   Input: ground truth, predition
 	"""
+	for i in range(0,len(y)):
+		print(y[i],pred[i])
 	output = np.array(pred)
 	scored = output - np.array(y)
 
@@ -25,7 +27,7 @@ def tolAcc(y,pred):
 	correct=0
 	c = Counter(scored)
 	for k in c.keys():
-		if k<2 and k>-2:
+		if k<1.5 and k>-1.5:
 			correct += c[k]
 	
 	score = float(correct)/len(scored)
@@ -46,23 +48,92 @@ def fiPlot(rf):
 
 
 
-def main():
+def main(argv):
 	X=np.load('numdata/epochFeats.npy')
-	Y=np.load('numdata/epochLabels.npy')
-	print(X.shape, Y.shape)
-	labels= np.load('numdata/LOO.npy')
-	folds=3
+	Y=np.transpose(np.array([np.load('numdata/epochLabels.npy')]))
+	labels= np.transpose(np.array([np.load('numdata/LOO.npy')]))
 
-	#Pipeline stuff 
-	forest = RandomForestClassifier(n_estimators=100, n_jobs = -1)
-	scaler = preprocessing.StandardScaler()
-	lolo = LeaveOneLabelOut(labels)
-	kf = StratifiedKFold(Y,n_folds=folds)
+	if sys.argv[1]=='-first':
+		
+		print(X.shape, Y.shape, labels.shape)
+		folds=3
 
-	#us = UnderSampler(verbose=True)
+		#Pipeline stuff 
+		forest = RandomForestRegressor(n_estimators=100, n_jobs = -1)
+		scaler = preprocessing.StandardScaler()
+
+		lolo = LeaveOneLabelOut(labels)
+		
+		acc = 0
+
+		us = UnderSampler(verbose=True)
+		X,Y = us.fit_transform(X,Y)
+		kf = StratifiedKFold(Y,n_folds=folds)
+		for train_index,test_index in kf:
+		#	print max(train_index),max(test_index)
+			Xtrain,Xtest = X[train_index], X[test_index]
+			ytrain,ytest = Y[train_index], Y[test_index]
+			
+			forest.fit(Xtrain,ytrain)
+
+
+			scores = forest.predict(Xtest)
+			acc += tolAcc(ytest,scores)
+			
+		print(acc/folds)
+	
+	elif sys.argv[1]=='-ensemble':
+		RF =[]
+		
+		print(X.shape,Y.shape,labels.shape)
+		# concatenating user labels to distinguish which rows correspond to which user
+		alltogether = np.concatenate((X,Y,labels),axis=1)
+		print(alltogether.shape)
+		for testUser in uids1:
+			# separating user-specific data----------------------if u(XX) in Labels keep it
+			trainMat = np.array([item[:] for item in alltogether if item[-1]==testUser[-2:]])
+			
+			# splitting again to keep only feature dataset, user labels not needed any more
+			trainMat = trainMat[: , 0:trainMat.shape[1]-1]
+
+			# separating features into categories for Ensemble Training
+			activityData = trainMat[:,0:6 ]
+			screenData = trainMat[:,6:17]
+			conversationData = trainMat[:,17:23 ]
+			colocationData = trainMat[:,23:trainMat.shape[1]-1]
+			Y = trainMat[:,trainMat.shape[1]-1:trainMat.shape[1]]
+			i=0
+			#Training 4 regressors
+			for data in [activityData,screenData,conversationData,colocationData]:
+				RF.append(RandomForestRegressor(n_estimators=300,max_features=None,n_jobs=-1))
+				RF[i].fit(data,Y)
+				i += 1
+			
+			layers_all = [('input',InputLayer),
+				   ('dense',DenseLayer),
+				   	('output',DenseLayer)]
+
+			net = NeuralNet(layers = layers_all,
+ 					 input_shape = (None,len(RF)),
+					 dense_num_units=5,
+					 dense_nonlinearity=None,
+					 regression=True,
+					 update_momentum=0.9,
+					 update_learning_rate=0.001,
+	 				 output_nonlinearity=None,
+ 					 output_num_units=1,
+ 					 max_epochs=100)
+
+
+			print(activityData.shape, screenData.shape, conversationData.shape, colocationData.shape, Y.shape	)
+
+
+	
+
+"""
 #	pipe = Pipeline(steps=[('scaler',scaler),('pca',pca) ,('forest',forest)])
 	bestNComp=[]
-	for comp in range(2,25):
+	for comp in range(2,25):	
 		pca= PCA(n_components=comp)
 		pipe = Pipeline(steps=[('scaler',scaler),('pca',pca) ,('forest',forest)])
 
@@ -72,22 +143,8 @@ def main():
 	maxA= max([item[0] for item in bestNComp])
 	print('Optimal number of Principal Components is: {0} with according accuracy of : {1}'.format(maxC,maxA))
 #	X = preprocessing.scale(X)
-	#X,Y = us.fit_transform(X,Y)
 
-	"""
-	
-
-	for train_index,test_index in kf:
-		Xtrain,Xtest = X[train_index], X[test_index]
-		ytrain,ytest = Y[train_index], Y[test_index]
-		forest.fit(Xtrain,ytrain)
-
-
-		scores = forest.predict(Xtest)
-		print(tolAcc(ytest,scores))
-	"""
-
-
+"""
 
 
 
