185cd5f87493769d152ac9c815443b9b3ad3aecf
stavskal <stavrianos_@hotmail.com>
2015-12-17 13:56:03 +0100

    Added bounding box calculations for DBSCAN clusters, saved to .npy file

--- visualizations/importantPlaces.py
+++ visualizations/importantPlaces.py
@@ -11,44 +11,57 @@ from geopy.distance import great_circle
 from sklearn import neighbors
 from sklearn.metrics.pairwise import pairwise_distances
 
-
-def distMatrix(X):
-    distM = np.zeros((X.shape[0],X.shape[0]))
-    for i in range(0,X.shape[0]):
-        for j in range(0,X.shape[0]):
-            if j>i:
-                d = great_circle(X[i,:],X[j,:]).meters
-                distM[i,j] = d
-    np.save('distmat.npy',distM)
-    print(distM)
-
 uids1=['u16','u19','u44','u24']
 
-n_clusters = 5
+
 X = []
+# Connecting to DB to fetch data
 con = psycopg2.connect(database='dataset', user='tabrianos')
 cur = con.cursor()
 
 for u in uids1:
     cur.execute('SELECT latitude,longitude FROM {0} WHERE longitude<=-72.2'.format(u+'gpsdata'))
     X += cur.fetchall()
-print(type(X))
-
+# Converting to numpy for compatibility with sklearn
 X = np.array(X)
-print(X.shape)
 
 # Setting DBSCAN parameters and clustering student data
-db = DBSCAN(eps=0.002, min_samples=8,metric='haversine', algorithm='auto').fit(X)
+db = DBSCAN(eps=0.003, min_samples=8,metric='haversine', algorithm='auto').fit(X)
+y = DBSCAN(eps=0.003, min_samples=8,metric='haversine', algorithm='auto').fit_predict(X)
+
+
+
+
+
+
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 core_samples_mask[db.core_sample_indices_] = True
+print(db.labels_)
 labels = db.labels_
 
 # Number of clusters in labels, ignoring noise if present.
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-
 print('Estimated number of clusters: %d' % n_clusters_)
 
-# Plot result
+
+bbox = np.zeros((n_clusters_,4))
+#Discovering Bounding Box coordinates (kinda sloppy but works)
+for i in range(0,n_clusters_):
+    coords=[]
+    for j in range(0,len(y)):
+        if y[j]==i:
+            coords.append(X[j,:])
+    # coords holds all gps coordinate pairs that correspond to one label per loop
+    coords = np.array(coords)
+    #          min lat, min long        max lat, max long
+    bbox[i,:]=[min(coords[:,0]),min(coords[:,1]),max(coords[:,0]),max(coords[:,1])]
+
+# save for further use
+np.save('bbox.npy',bbox)
+
+
+
+# Plot results
 import matplotlib.pyplot as plt
 
 # Black removed and is used for noise instead.
@@ -60,14 +73,16 @@ for k, col in zip(unique_labels, colors):
         col = 'k'
 
     class_member_mask = (labels == k)
+    print(class_member_mask)
 
     xy = X[class_member_mask & core_samples_mask]
     plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
-             markeredgecolor='k', markersize=14)
+             markeredgecolor='k', markersize=8)
 
     xy = X[class_member_mask & ~core_samples_mask]
     plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
-             markeredgecolor='k', markersize=6)
-
+             markeredgecolor='k', markersize=4)
+plt.ylabel('Longitude')
+plt.xlabel('Latitude')
 plt.title('Estimated number of clusters: %d' % n_clusters_)
 plt.savefig('dbscan.png')
