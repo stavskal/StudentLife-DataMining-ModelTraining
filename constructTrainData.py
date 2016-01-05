0ca5e5b72a00efcff95ca935d751019f221d7d9a
stavskal <stavrianos_@hotmail.com>
2016-01-05 16:19:59 +0100

    Added gpsFeatures to Train Matrix saved

--- constructTrainData.py
+++ constructTrainData.py
@@ -2,6 +2,7 @@ import json,csv,sys,os,psycopg2,random
 import numpy as np
 from collections import Counter 
 from processingFunctions import *
+from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_score, recall_score, confusion_matrix
 from sklearn.cross_validation import cross_val_score, StratifiedKFold
@@ -22,9 +23,8 @@ uids = ['u00','u01','u02','u03','u04','u05','u07','u08','u09','u10','u12','u13',
 'u56','u57','u58']
 
 #uids1=['u59','u57','u02','u52','u16','u19','u44','u24','u51','u00','u08']
-#uids2=['u59','u57','u02','u52','u16','u19','u44','u24','u51','u00','u08']
-uids1=['u16','u19','u44','u24','u08','u51','u59','u57','u00','u02','u52','u10','u32','u33','u43','u49']
-uids2=['u16','u19','u44','u24','u08','u51','u59','u57','u00','u02','u52','u10','u32','u33','u43','u49']
+#uids2=['u02','u00','u57']
+uids2=['u44','u24','u08','u51','u59','u57','u00','u02','u52','u10','u32','u33','u43','u49','u16','u19']
 
 ch = [120,100,70,50,35]
 
@@ -49,29 +49,32 @@ def main():
 	con = psycopg2.connect(database='dataset', user='tabrianos')
 	cur = con.cursor()
 	#warnings.simplefilter("error")
+	centers = np.load('visualizations/clustercenters.npy')
 
 # ------------TEST CASE-----------------------------
 	for loso in uids1:
 		ytest=[]
 		accuracies =[]
 		acc=0
-		totalP=0
-		totalR=0
 		maxminAcc =[]
-		Xbig = np.zeros([1,26])	
-		Ybig = np.ones([1])
+		Xbig = np.zeros([1,34])	
+		Ybig = np.zeros([1])
+		labels=[]
+		labels.append(19)
 		# loso means leave one student out: forest is trained on other users data
 		# then tests are run on 'loso' student 
 		uids2.remove(loso)
 		uids2.append(loso)
 		print('LOSO: {0}'.format(loso))
 		for testUser in uids2:
-
+			print(testUser)
 			# lists that temporary store features before concatenation
 			ScreenList = []
 			colocationList =[]
 			conversationList =[]
 			activityList=[]
+			audioList = []
+			gpsList = []
 
 			# loading stress labels from database (currently on 0-5 scale)
 			records = loadStressLabels(cur,testUser) 
@@ -83,8 +86,8 @@ def main():
 			X = np.array(records)
 
 			# X is shuffled twice to ensure that the report sequence is close to random
-			np.random.shuffle(X)
-			np.random.shuffle(X)
+			#np.random.shuffle(X)
+			#np.random.shuffle(X)
 
 
 			for i in range(0,len(records)):
@@ -92,34 +95,47 @@ def main():
 				conversationList.append( convEpochFeats(cur,testUser,X[i][0]))
 				activityList.append(activityEpochFeats(cur,testUser,X[i][0]))
 				ScreenList.append( screenStatFeatures(cur,testUser,X[i][0],day) )
-
+				audioList.append(audioEpochFeats(cur,testUser,X[i][0]))
+				gpsList.append(gpsFeats(cur,testUser,X[i][0],centers))
+				#print(gpsFeats(cur,testUser,X[i][0],centers))
 				if testUser==loso:
 					ytest.append(X[i][1])
-
+				#labels list holds user ids to be used in LeaveOneOut pipeline
+				labels.append(testUser[-2:])
 				Y[i] = X[i][1]
 
 			
 
 			#concatenating features in one array 
-			Xtt = np.concatenate((np.array(activityList),np.array(ScreenList),np.array(conversationList),np.array(colocationList)),axis=1)
-			#print(Xtt.shape)
+			Xtt = np.concatenate((np.array(activityList),np.array(ScreenList),np.array(conversationList),np.array(colocationList),np.array(audioList),np.array(gpsList)),axis=1)
+			print(Xtt.shape)
 
 			#initiating and training forest, n_jobs indicates threads, -1 means all available
 			# while the test student is not reached, training data are merged into one big matrix
+			Xbig = np.concatenate((Xbig,Xtt),axis=0)
+			Ybig = np.concatenate((Ybig,Y),axis=0)
+			#print(Xbig.shape[0],len(labels))
+			#print(Xbig.shape[0],Ybig.shape[0],len(labels))
+
 			if testUser!=loso:
-				Xbig = np.concatenate((Xbig,Xtt),axis=0)
-				Ybig = np.concatenate((Ybig,Y),axis=0)
+				#Xbig = np.concatenate((Xbig,Xtt),axis=0)
+				#Ybig = np.concatenate((Ybig,Y),axis=0)
 				
 				Xbig = Xbig.astype(np.float64)
-				forest = RandomForestClassifier(n_estimators=100, n_jobs = -1)
-				forest.fit(Xbig,Ybig)
+				
 				
 
-			# when loso, test are run
+			# when loso, tests are run
 			elif testUser==loso:
-				np.save('epochFeats.npy',Xbig)
-				np.save('epochLabels.npy',Ybig)
+				Xbig = preprocessing.scale(Xbig)
+				np.save('numdata/withgps/epochFeats.npy',Xbig)
+				np.save('numdata/withgps/epochLabels.npy',Ybig)
+				np.save('numdata/withgps/LOO.npy',np.array(labels))
+				print(Xbig.shape[0],Ybig.shape[0],len(labels))				
 				print('train matrix saved')
+				a = raw_input()
+				forest = RandomForestClassifier(n_estimators=100, n_jobs = -1)
+				forest.fit(Xbig,Ybig)
 				ef = forest.score(Xtt,ytest)
 				print(ef*100)
 
