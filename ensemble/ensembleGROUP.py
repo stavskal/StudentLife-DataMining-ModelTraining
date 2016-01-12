e7d93ea9f556eb015346e1e50761da4ec13961b7
stavskal <stavrianos_@hotmail.com>
2016-01-12 13:56:42 +0100

    Group tests after ADASYN oversampling (works like a charm)

---
+++ ensemble/ensembleGROUP.py
@@ -0,0 +1,121 @@
+import json,csv,sys,os,psycopg2,random
+import numpy as np
+from collections import Counter 
+from processingFunctions import *
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor, ExtraTreesClassifier
+from sklearn.metrics import precision_score, recall_score, confusion_matrix
+from sklearn.cross_validation import cross_val_predict, StratifiedKFold, KFold,cross_val_score, LeaveOneLabelOut
+from sklearn.pipeline import Pipeline
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+import time
+import warnings
+
+from pipeTrain import deleteClass
+
+sys.path.insert(0,'//home/tabrianos/Desktop/Thesis/Database/venv/ADASYN/ADASYN')
+
+from adasyn import ADASYN
+
+def tolAcc(y,pred):
+	"""Returns accuracy as defined by the Tolerance Method
+	   Input: ground truth, prediction
+	"""
+	correct=0
+	truecorrect=0
+	errors=[]
+	for i in range(0,len(y)):
+		errors.append(np.absolute(y[i]-pred[i]))
+	#	print('Pred,True: {0},{1} Data: {2}'.format(pred[i],y[i],testMat[i,:]))
+		if errors[i]<=1:
+			correct+=1
+		if errors[i]==0:
+			truecorrect+=1
+	score = float(correct)/len(y)
+	truescore = float(truecorrect)/len(y)
+	meanEr = sum(errors)/len(errors)
+	#print('Mean error: {0}'.format(meanEr))
+	#print('Min max prediction: {0},{1}'.format(min(pred),max(pred)))
+	y = y.astype(np.int64)
+	pred = pred.astype(np.int64)
+	#Number of predictions in each class
+	print('              Ground Truth ------------ Prediction')
+	print(np.bincount(y),np.bincount(pred))
+	return(score*100)
+
+def fiPlot(rf):
+	""" Bar plot of feature importances of RF
+	"""
+	fi = rf.feature_importances_
+	print(len(fi))
+	fi = 100* (fi/fi.max())
+	sorted_idx = np.argsort(fi)
+	pos = np.arange(len(fi))
+	print(pos)
+	plt.figure()
+	plt.barh(pos,fi[sorted_idx],align='center')
+	plt.savefig('featureImporances.png')
+
+
+
+def main(argv):
+	X=np.load('numdata/withgps/epochFeats.npy')
+	Y=np.load('numdata/withgps/epochLabels.npy')
+	labels= np.load('numdata/withgps/LOO.npy')
+
+	#fixes errors with Nan data
+	X = preprocessing.Imputer().fit_transform(X)
+
+	#BABY IM WORTH IS NANANANAN
+	adsn = ADASYN(ratio=0.5)
+	X,Y = adsn.fit_transform(X,Y)
+	print(X.shape,Y.shape)
+
+
+	# Ensemble Random Forest Regressor stacked with Random Forest Classifier
+	if sys.argv[1]=='-ensemble':
+		RF  = []
+		outputRF = []
+		outRFtest=[]
+		totalacc=0
+	
+		print(X.shape,Y.shape)
+
+		# separating features into categories for Ensemble Training
+		activityData = X[:,0:3 ]
+		screenData = X[:,3:14]
+		conversationData = X[:,14:20 ]
+		colocationData = X[:,20:26]
+		audioData = X[:,26:X.shape[1]]
+
+		skf = StratifiedKFold(Y,n_folds=3)
+
+		for traini, testi in skf:
+			print(len(traini),len(testi))
+			#np.random.shuffle(traini)
+			# separating train data to 2 subsets: 
+			# 1) Train RFR(60% of data)
+			# 2) Get RF outputs with which train RF classifier (40% of data)
+			#train_index = traini[0: int(0.6*len(traini))]
+			#train_index2 =  traini[int(0.6*len(traini)):len(traini)]
+
+			class_weights={0 : 1, 1 : 0.4 , 2 : 0.1 , 3 : 0.4, 4 :1}
+			rfr= RandomForestClassifier(n_estimators=300,class_weight='auto',n_jobs=-1)
+
+			rfr.fit(X[traini],Y[traini])
+			pred = rfr.predict(X[testi])
+
+			# Print to screen mean error and Tolerance Score
+			tempacc = tolAcc(Y[testi],pred)
+			print(tempacc)
+			totalacc += tempacc
+			#print(rfr.feature_importances_)
+
+		print('LOSO TP accuracy: {0}'.format(totalacc/3))
+
+
+
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
