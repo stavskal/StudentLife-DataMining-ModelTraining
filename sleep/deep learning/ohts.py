22eb0aed0fd2a01cd0ff5de4aebf01c82dccfa46
stavskal <stavrianos_@hotmail.com>
2016-03-17 16:26:15 +0100

    One hot encoding of tuple timeseries

--- sleep/deep learning/ohts.py
+++ sleep/deep learning/ohts.py
@@ -30,6 +30,7 @@ class OneHotTimeSeries(object):
 		self.set_period()
 		self.data_retrieval()
 		self.align_timeseries()
+		self.one_hot_encode()
 
 	def set_period(self):
 		"""
@@ -62,11 +63,21 @@ class OneHotTimeSeries(object):
 		self.cur.execute('SELECT * FROM {0} WHERE time_stamp>={1} AND time_stamp<={2}'
 			.format(self.user+'act', self.pm10, self.am10) )
 		records = self.cur.fetchall()
+		if not records:
+			print('No activity data found')
+			exit()
+		# Accelerometer / audio classifier also classified as 'Unknown' (3rd class)
+		# which does not provide any context, so discarding
+		records = [item for item in records if item[1]!=3]
 		self.activity_ts = {timestamp: inference for timestamp,inference in records}
 
 		self.cur.execute('SELECT * FROM {0} WHERE time_stamp>={1} AND time_stamp<={2}'
 			.format(self.user+'audio', self.pm10, self.am10) )
 		records = self.cur.fetchall()
+		if not records:
+			print('No audio data found')
+			exit()
+		records = [item for item in records if item[1]!=3]
 		self.audio_ts = {timestamp: inference for timestamp,inference in records}
 
 
@@ -74,10 +85,10 @@ class OneHotTimeSeries(object):
 		"""
 		Creates tuple time series with fixed length containing
 		(activity,audio) data for every timestemp. Step in time is
-		5 seconds atm
+		5 seconds atm, 86400 values in 12 hour period
 		"""
 		print(self.pm10,self.am10)
-		tuple_ts = np.zeros([2,8640])
+		self.tuple_ts = np.zeros([2,8640])
 		current_time = self.pm10
 		time_list_ac = np.array(self.activity_ts.keys())
 		time_list_au = np.array(self.audio_ts.keys())
@@ -91,23 +102,47 @@ class OneHotTimeSeries(object):
 
 			nearest_ac_inference = np.argmin(diff_ac)
 			nearest_au_inference = np.argmin(diff_au)
+
 			# Keeping error for evaluation purposes
 			distances_ac[i] = (time_list_ac[nearest_ac_inference]-current_time)
 			distances_au[i] = (time_list_au[nearest_au_inference]-current_time)
+
 			# Timelist[nearest] is the nearest timestamp, which is also the key
 			# to get the inference from activity/audio_(ts)
-			tuple_ts[0,i] = self.activity_ts[time_list_ac[nearest_ac_inference]]
-			tuple_ts[1,i] = self.audio_ts[time_list_au[nearest_au_inference]]
-		
+			self.tuple_ts[0,i] = self.activity_ts[time_list_ac[nearest_ac_inference]]
+			self.tuple_ts[1,i] = self.audio_ts[time_list_au[nearest_au_inference]]
 			current_time += 5
 		
 		print(np.mean(distances_ac), np.median(distances_ac))
 		print(np.mean(distances_au), np.median(distances_au))
+		print(self.tuple_ts[:,100])
+		print(self.tuple_ts[:,200])
 
+	def one_hot_encode(self):
+		self.oht = np.zeros([9,8640])
+		for i in range(0,8640):
+			if self.tuple_ts[0,i]==0 and self.tuple_ts[1,i]==0:
+				self.oht[0,i] = 1
+			elif self.tuple_ts[0,i]==0 and self.tuple_ts[1,i]==1:
+				self.oht[1,i] = 1
+			elif self.tuple_ts[0,i]==0 and self.tuple_ts[1,i]==2:
+				self.oht[2,i] = 1
+			elif self.tuple_ts[0,i]==1 and self.tuple_ts[1,i]==0:
+				self.oht[3,i] = 1
+			elif self.tuple_ts[0,i]==1 and self.tuple_ts[1,i]==1:
+				self.oht[4,i] = 1
+			elif self.tuple_ts[0,i]==1 and self.tuple_ts[1,i]==2:
+				self.oht[5,i] = 1
+			elif self.tuple_ts[0,i]==2 and self.tuple_ts[1,i]==0:
+				self.oht[6,i] = 1
+			elif self.tuple_ts[0,i]==2 and self.tuple_ts[1,i]==1:
+				self.oht[7,i] = 1
+			elif self.tuple_ts[0,i]==2 and self.tuple_ts[1,i]==2:
+				self.oht[8,i] = 1
 
-
-
-
+		print(self.oht[:,100])
+		print(self.oht[:,200])
+		print(self.oht.shape)
 
 
 	#def time_interval12(self):
@@ -119,5 +154,5 @@ except psycopg2.DatabaseError as err:
 	print('Error %s' % err)
 	exit()
 
-a = OneHotTimeSeries(cur=cur, user='u00', response_ts=1366655156, rate=1)
+a = OneHotTimeSeries(cur=cur, user='u00', response_ts=1364580795, rate=1)
 print(a.am10-a.pm10)
